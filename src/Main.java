e9f2e02b639a2852c1428079883ae99db541e379
Setlocal1989 <denis1989g@mail.ru>
2023-05-23 11:26:33 +0400

    generics

--- src/Main.java
+++ src/Main.java
@@ -3,20 +3,6 @@ import generics.Generics;
 public class Main {
     public static void main(String[] args) {
         Generics<String> test = new Generics<>();
-//        for (int i = 0; i < 101; i++) {
-//            System.out.println("test.add(\" \" + " + i + ") = " + test.add("a"));
-//        }
-        test.add("0");
-        test.add("1");
-        test.add("2");
-        test.add("3");
-        System.out.println("test.size() = " + test.size());
-//        test.remove(4);
-        test.add("4",3);
-
-        for (int i = 0; i < test.size(); i++) {
-            System.out.println(test.get(i));
-        }
 
     }
 }
